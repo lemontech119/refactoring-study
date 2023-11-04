18b2d1569abdcc972858f02599989e8de43d734b
AhnDongwon <lemontech119@gmail.com>
2023-11-04 15:34:42 +0900

    11장 1번

--- src/chapter11-1/src/app/app.component.ts
+++ src/chapter11-1/src/app/app.component.ts
@@ -1,114 +1,160 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TStore, TTable, TReservation, store, tables } from './db';
+
+export class ReservationService {
+  private _tables: TTable[] = [];
+  private _reservations: TReservation[] = [];
+
+  constructor() {}
+
+  get tables(): TTable[] {
+    return this._tables;
+  }
+
+  get reservations(): TReservation[] {
+    return this._reservations;
+  }
+
+  setTables(tables: TTable[]): void {
+    this._tables = tables;
+  }
+
+  findAvailableTable(peopleCount: number): TTable | undefined {
+    return this._tables.find(
+      (table) => !table.isUsing && table.maxCapacity >= peopleCount
+    );
+  }
+
+  addReservation(peopleCount: number, phoneNumber: string): void {
+    this._reservations.push({
+      id: this._reservations.length + 1,
+      peopleCount,
+      phoneNumber,
+    });
+  }
+
+  cancelReservation(reservationId: number): void {
+    const index = this._reservations.findIndex(
+      (reservation) => reservation.id === reservationId
+    );
+    if (index !== -1) {
+      this._reservations.splice(index, 1);
+    }
+  }
+
+  openReservation(table: TTable): boolean {
+    if (this._reservations.length > 0) {
+      const reservation = this._reservations.shift();
+      if (reservation) {
+        table.isUsing = true;
+        return true;
+      }
+    }
+    return false;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public store: TStore = store;
   public tables: TTable[] = tables;
-  public reservations: TReservation[] = [];
   public peopleCount = 0;
   public phoneNumber = '';
   public maxCapacity = 0;
   public location = '';
+  public reservation = new ReservationService();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.reservation.setTables(tables);
+  }
 
   showAlert(message: string): void {
     alert(message);
   }
 
+  resetTablingView(message: string): void {
+    this.peopleCount = 0;
+    this.phoneNumber = '';
+    this.showAlert(message);
+  }
+
+  resetAddTableView(message: string): void {
+    this.maxCapacity = 0;
+    this.location = '';
+    this.showAlert(message);
+  }
+
   doTabling(peopleCount: number, phoneNumber: string): void {
-    let message = '';
     if (peopleCount <= 0 || phoneNumber === '') {
-      message = '인원수와 전화번호를 입력해주세요.';
-      this.showAlert(message);
+      this.showAlert('인원수와 전화번호를 입력해주세요.');
       return;
     }
 
-    const table = this.tables.find(
-      (table) => table.isUsing === false && table.maxCapacity >= peopleCount
-    );
+    const table = this.reservation.findAvailableTable(peopleCount);
 
     if (table) {
       table.isUsing = true;
-      message = `${table.location} 테이블에 앉으세요.`;
-      this.peopleCount = 0;
-      this.phoneNumber = '';
-      this.showAlert(message);
-    } else {
-      this.reservations.push({
-        id: this.reservations.length + 1,
-        peopleCount,
-        phoneNumber,
-      });
-      message = '예약이 완료 됐습니다.';
-      this.peopleCount = 0;
-      this.phoneNumber = '';
-      this.showAlert(message);
+      this.resetTablingView(`${table.location} 테이블에 앉으세요.`);
+      return;
     }
+    this.reservation.addReservation(peopleCount, phoneNumber);
+    this.resetTablingView('예약이 완료 됐습니다.');
   }
 
   cancelReservation(reservationId: number): void {
-    const index = this.reservations.findIndex(
-      (reservation) => reservation.id === reservationId
-    );
-    if (index !== -1) {
-      this.reservations.splice(index, 1);
-    }
+    this.reservation.cancelReservation(reservationId);
   }
 
   endTable(tableId: number): void {
-    const table = this.tables.find((table) => table.id === tableId);
-    if (table) {
-      table.isUsing = false;
-      if (this.reservations.length > 0) {
-        const reservation = this.reservations.shift();
-        if (reservation) {
-          table.isUsing = true;
-          this.showAlert(
-            `${reservation.phoneNumber}님, ${table.location} 테이블에 앉으세요.`
-          );
-        }
-      }
+    const table = this.reservation.tables.find((table) => table.id === tableId);
+    if (!table) {
+      return;
+    }
+
+    table.isUsing = false;
+    if (this.reservation.openReservation(table)) {
+      this.showAlert(`${table.location} 테이블에 앉으세요.`);
     }
   }
 
   addTable(storeId: number, maxCapacity: number, location: string): void {
-    this.tables.push({
-      id: this.tables.length + 1,
+    const newTable: TTable = {
+      id: this.reservation.tables.length + 1,
       storeId,
       maxCapacity,
       location,
       isUsing: false,
-    });
+    };
+    const tables = this.reservation.tables;
+    tables.push(newTable);
+    this.reservation.setTables(tables);
 
-    this.store.tableCount = this.tables.length;
-    this.peopleCount = 0;
-    this.phoneNumber = '';
-
-    this.showAlert(`${location} 테이블이 추가 됐습니다.`);
+    this.store.tableCount = this.reservation.tables.length;
+    this.resetAddTableView(`${location} 테이블이 추가 됐습니다.`);
   }
 
   removeTable(tableId: number): void {
-    let message = '';
-    const table = this.tables.find((table) => table.id === tableId);
-    if (table) {
-      if (table.isUsing) {
-        message = '사용중인 테이블은 삭제할 수 없습니다.';
-        this.showAlert(message);
-        return;
-      }
-      const index = this.tables.findIndex((table) => table.id === tableId);
-      if (index !== -1) {
-        this.tables.splice(index, 1);
-        message = `${table.location} 테이블이 삭제 됐습니다.`;
-        this.showAlert(message);
-      }
-    } else {
-      let message = '테이블이 없습니다.';
-      this.showAlert(message);
+    const tableList = this.reservation.tables;
+    const table = tableList.find((table) => table.id === tableId);
+    if (!table) {
+      this.showAlert('테이블이 없습니다.');
+      return;
+    }
+    if (table.isUsing) {
+      this.showAlert('사용중인 테이블은 삭제할 수 없습니다.');
+      return;
+    }
+    const index = tableList.findIndex((table) => table.id === tableId);
+    if (index !== -1) {
+      tableList.splice(index, 1);
+      this.reservation.setTables(tableList);
+      this.showAlert(`${table.location} 테이블이 삭제 됐습니다.`);
     }
   }
 }
